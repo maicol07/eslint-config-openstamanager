ae2753cc55138a85d598d54935b194b2d26586c5
Maicol Battistini <maicolbattistini@live.it>
2022-01-04 11:03:50 +0100

    feat: ✨ Replaced Adeira config with AirBNB config

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import type {ParserOptions} from "@typescript-eslint/parser";
+/* eslint-disable unicorn/prefer-module, import/no-import-module-exports */
+import type { ParserOptions } from '@typescript-eslint/parser';
 
 const parserOptions: ParserOptions = {
   ecmaFeatures: {
-    jsx: true,
+    jsx: true
   },
   ecmaVersion: 'latest',
   project: ['./tsconfig.json'],
@@ -18,78 +19,77 @@ module.exports = {
     es2021: true
   },
   parser: '@typescript-eslint/parser',
-  parserOptions: parserOptions,
+  parserOptions,
   plugins: [
-    "@typescript-eslint",
-    "new-with-error",
-    "simple-import-sort"
+    '@typescript-eslint',
+    'new-with-error',
+    'simple-import-sort'
   ],
   extends: [
-    "@adeira/eslint-config/base",
-    "plugin:wc/recommended",
-    "plugin:lit/recommended",
-    "plugin:mithril/recommended",
-    "plugin:regexp/recommended",
-    "plugin:sonarjs/recommended",
-    "plugin:unicorn/recommended",
-    "plugin:you-dont-need-lodash-underscore/compatible",
-    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:wc/recommended',
+    'plugin:lit/recommended',
+    'plugin:mithril/recommended',
+    'plugin:regexp/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:you-dont-need-lodash-underscore/compatible',
     'plugin:@typescript-eslint/recommended-requiring-type-checking'
   ],
   globals: {
-    m: "readonly",
+    m: 'readonly',
     $: true
   },
   rules: {
-    "capitalized-comments": [
-      "error",
-      "always",
+    'capitalized-comments': [
+      'error',
+      'always',
       {
-        "ignorePattern": "pragma|ignored|noinspection",
-        "ignoreInlineComments": true
+        ignorePattern: 'pragma|ignored|noinspection',
+        ignoreInlineComments: true
       }
     ],
-    "comma-dangle": [
-      "error",
-      "never"
-    ],
-    "class-methods-use-this": "off",
-    "eol-last": "off",
-    "func-names": "off",
-    "lines-between-class-members": ["error", "always", {exceptAfterSingleLine: true}],
-    "no-invalid-this": "off",
-    "no-multiple-empty-lines": "off",
-    "no-param-reassign": ["error", {props: false}],
-    "no-restricted-syntax": "off",
-    "no-underscore-dangle": "off",
-    "no-unused-vars": ["error", {"vars": "all", "args": "none", "ignoreRestSiblings": false}],
-    "object-curly-spacing": "off",
-    "sort-imports": "off", // See simple-import-sort plugin
+    'comma-dangle': 'off',
+    'class-methods-use-this': 'off',
+    'eol-last': 'off',
+    'func-names': 'off',
+    'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+    'no-invalid-this': 'off',
+    'no-multiple-empty-lines': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-restricted-syntax': 'off',
+    'no-underscore-dangle': 'off',
+    'no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: false }],
+    'object-curly-spacing': 'off',
+    'sort-imports': 'off', // See simple-import-sort plugin
 
     // Plugins
-    //"custom-elements/extends-correct-class": "off", // Conflicts when extending other Web Components
-    "filenames/match-regex": "off",
-    "i18n-text/no-en": "off",
-    "import/extensions": ["error",
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
+    // "custom-elements/extends-correct-class": "off",
+    // Conflicts when extending other Web Components
+    'filenames/match-regex': 'off',
+    'i18n-text/no-en': 'off',
+    'import/extensions': ['error',
       {
-        js: "never",
-        ts: "never",
-        jsx: "never",
-        tsx: "never",
-        json: "always",
-        png: "always",
-        css: "always",
-        scss: "always"
+        js: 'never',
+        ts: 'never',
+        jsx: 'never',
+        tsx: 'never',
+        json: 'always',
+        png: 'always',
+        css: 'always',
+        scss: 'always'
       }
     ], // Allow imports to modules without extension
-    "import/no-unresolved": "off", // This rule doesn't work with relative imports
-    "import/order": "off", // Already handled (better) by simple-import-sort/imports
-    "mithril/jsx-no-target-blank": "off",
-    "n/file-extension-in-import": "off",
-    "new-with-error/new-with-error": "error",
-    "prettier/prettier": "off",
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "unicorn/filename-case": "off"
+    'import/no-unresolved': 'off', // This rule doesn't work with relative imports
+    'import/order': 'off', // Already handled (better) by simple-import-sort/imports
+    'mithril/jsx-no-target-blank': 'off',
+    'n/file-extension-in-import': 'off',
+    'new-with-error/new-with-error': 'error',
+    'prettier/prettier': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'unicorn/filename-case': 'off'
   }
 };
