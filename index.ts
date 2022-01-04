22ac42f5fd93ccf5bba307956092d60cc92c3f26
Maicol Battistini <maicolbattistini@live.it>
2022-01-04 11:48:40 +0100

    fix: 🐛 Disabled rule about global Mithril

--- index.ts
+++ index.ts
@@ -89,6 +89,7 @@ module.exports = {
     'import/no-unresolved': 'off', // This rule doesn't work with relative imports
     'import/order': 'off', // Already handled (better) by simple-import-sort/imports
     'mithril/jsx-no-target-blank': 'off',
+    'mithril/jsx-uses-mithril': 'off', // Will be declared as a global variable
     'n/file-extension-in-import': 'off',
     'new-with-error/new-with-error': 'error',
     'prettier/prettier': 'off',
